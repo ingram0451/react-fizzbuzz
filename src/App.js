3ffe6a5a2de75053edce67785054c2b2e7747ae7
ingram0451 <ingram0451@github.com>
2018-09-17 19:28:03 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: '',
+      hasError: false
+    };
+  }
+  
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    console.log(error);
+    console.log(info);
+  }
+
+
+  getResult(event) {
+    event.preventDefault();
+    const options = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',                  
+      },
+      body: JSON.stringify({'number': this.refs.input.value})
+    };
+
+    const request = new Request('http://localhost:8000/fizzbuzz/api', options);
+    fetch(request)
+      .then(resp => resp.text())
+      .then(text => {
+        this.setState({
+          result: text
+        })
+      }).catch(function(error) {
+        if (typeof repe == 'undefined'){
+          console.log('Sever is not responding');
+        } else
+          console.log(error);
+    });
+  }
+
+  render() {
+    const { result, hasError } = this.state;
+    return (
+      <div>
+        <form onSubmit={this.getResult.bind(this)}>
+          <input type="text" ref="input" placeholder="Please give a number..." />
+          <button type="submit">Go</button>
+        </form> 
+        { result && <div>Server says: {result}</div> }
+        { this.state.hasError && <div>Something went wrong.</div> }
+      </div>
+    );
+    
+  }
+
+}
+
+export default App;
